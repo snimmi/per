9384721908396ce3c8a6ac323ff98bd40837843a
snimmi <snimmi1609@gmail.com>
2024-08-08 14:36:07 +0530

    Add files via upload

---
+++ Amit.java
@@ -0,0 +1,14 @@
+package Package1;
+public class Amit
+{
+private int x;
+private int y;
+public void setData(int p ,int q) {
+x=p;
+y=q;
+}
+public void display(){
+System.out.println(x);
+System.out.println(y);
+}
+}
